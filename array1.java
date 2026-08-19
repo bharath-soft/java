a75297b30524b122e1148b4f8b72cfaacf6aca2d
bharath <bharathcse1122@gmail.com>
2026-08-19 20:37:19 +0530

    array1 added

---
+++ array1.java
@@ -0,0 +1,9 @@
+public class array1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+         {
+            System.out.println(arr[4]);
+        }
+    }
+    
+}
