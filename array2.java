af29ccdf9daf33fdd8efdab24f61be943688043d
bharath <bharathcse1122@gmail.com>
2026-08-20 20:58:10 +0530

    array11

---
+++ array2.java
@@ -0,0 +1,11 @@
+public class array2 {
+    public static void main(String[] args) {
+        String[] hotel = new String[100];
+        hotel[0] = "dosa";
+        hotel[1] = "idli";
+        hotel[2] = "vada";
+        System.out.println(hotel[0]);
+        System.out.println(hotel[1]);
+        System.out.println(hotel[2]);
+    }
+}
