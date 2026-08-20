d50bbbabce62598091fe663df2d5cf7486c121ad
bharath <bharathcse1122@gmail.com>
2026-08-20 21:13:38 +0530

    type 3 added

--- array3.java
+++ array3.java
@@ -1,3 +1,12 @@
+import java.util.Scanner;
 public class array3 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] adding = new int[5];
+        adding[0] = sc.nextInt();
+        adding[1] = sc.nextInt();
+        adding[2] = sc.nextInt();
+        System.out.println(adding[0]+adding[1]+adding[2]);
+       
+}
 }
