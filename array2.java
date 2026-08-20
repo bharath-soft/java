41ca05232f96f3338dcc4c52842a9b750dfa686a
bharath <bharathcse1122@gmail.com>
2026-08-20 21:04:07 +0530

    error sloved

--- array2.java
+++ array2.java
@@ -1,11 +1,13 @@
 public class array2 {
     public static void main(String[] args) {
-        String[] hotel = new String[100];
+        String[] hotel = new String[5];
         hotel[0] = "dosa";
         hotel[1] = "idli";
         hotel[2] = "vada";
         System.out.println(hotel[0]);
         System.out.println(hotel[1]);
         System.out.println(hotel[2]);
+        System.out.println(hotel[3]);
+        System.out.println(hotel[4]);
     }
 }
